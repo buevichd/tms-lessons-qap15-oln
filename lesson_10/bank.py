582186e28f905570e604bcef90604689a9092c9b
Dmitry Buevich <buevichd@gmail.com>
2023-10-23 21:16:21 +0300

    Lesson 10, class Bank (1)

--- lesson_10/bank.py
+++ lesson_10/bank.py
@@ -15,3 +15,17 @@ class BankAccount:
         self.account_number = get_random_digits(20)
         self.card_number = get_random_digits(16)
 
+
+class Bank:
+    def __init__(self):
+        self.bank_accounts: dict[str, BankAccount] = {}
+
+    def open_account(self, card_holder) -> BankAccount:
+        account = BankAccount(card_holder)
+        self.bank_accounts[account.account_number] = account
+        return account
+
+    def add_money(self, account_number: str, money: float):
+        target_account = self.bank_accounts[account_number]
+        target_account.money += money
+
